71175ba57fa7324f1f6c91bf9794f64bc1e16544
falfaqih <fslalfaqih@gmail.com>
2025-07-01 07:48:19 -0700

    CDK deployed, Lambda in progress

--- cdk/cdk/cdk_stack.py
+++ cdk/cdk/cdk_stack.py
@@ -14,13 +14,4 @@ class CdkStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        queue = sqs.Queue(
-            self, "CdkQueue",
-            visibility_timeout=Duration.seconds(300),
-        )
-
-        topic = sns.Topic(
-            self, "CdkTopic"
-        )
-
-        topic.add_subscription(subs.SqsSubscription(queue))
+        
